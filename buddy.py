059c2b18df8246038a10b3be3d091fb181bc9ba8
Alex <59406285+AlexKaos32@users.noreply.github.com>
2024-05-05 14:10:25 -0500

    Create buddy.py

---
+++ buddy.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-y", "--your_user", type=str, required=True, help="User you have creds for")
+parser.add_argument("-t", "--target_user", type=str, required=True, help="Target user")
+parser.add_argument("-r", "--range", type=int, default=100, help="This should be equal to len of original passlist")
+parser.add_argument("-m", "--pass_mode", action="store_true", help="Generate a password list with similar logic")
+parser.add_argument("-p", "--password", type=str, required=False, help="The password for your account")
+parser.add_argument("-l", "--list", required=False, help="Password list to modify")
+args = parser.parse_args()
+
+userlist = ""
+
+for u in range(0, args.range):
+    if (u + 1) % 3 ==0:
+        userlist += args.your_user + '\n'
+    else:
+        userlist += args.target_user + '\n'
+
+file_path = Path('new_users.txt')
+file_path.write_text(userlist)
+
+if args.pass_mode:
+    passlist = Path(args.list)
+
+    with passlist.open('r') as file:
+        lines = file.readlines()
+    
+    modded_list = ""
+    for p, line in enumerate(lines, start=1):
+        modded_list += str(p) + '\n'
+        if p % 2 == 0:
+            modded_list += args.password + '\n'
+    
+    plist = Path('new_passwords.txt')
+    plist.write_text(modded_list)
